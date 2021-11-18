96c6e8718f53f01a3e95e7d173225cd487ed7a60
Vishaldubbaka <vishalreddyusa@gmail.com>
2021-11-17 19:59:36 -0600

    changes done

--- controllers/suit.js
+++ controllers/suit.js
@@ -32,7 +32,7 @@ exports.suit_create_post =async function(req, res) {
     // We are looking for a body, since POST does not have query parameters. 
     // Even though bodies can be in many different formats, we will be picky 
     // and require that it be a json object 
-    // {"costume_type":"goat", "cost":12, "size":"large"} 
+    // {"suit_type":"goat", "cost":12, "size":"large"} 
     document.suit = req.body.suit; 
     document.size = req.body.size; 
     document.price = req.body.price; 
@@ -47,9 +47,18 @@ exports.suit_create_post =async function(req, res) {
 }; 
  
 // Handle Suit delete form on DELETE. 
-exports.suit_delete = function(req, res) { 
-    res.send('NOT IMPLEMENTED: Suit delete DELETE ' + req.params.id); 
-}; 
+// Handle suit delete on DELETE. 
+exports.suit_delete = async function(req, res) { 
+    console.log("delete "  + req.params.id) 
+    try { 
+        result = await Suit.findByIdAndDelete( req.params.id) 
+        console.log("Removed " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": Error deleting ${err}}`); 
+    } 
+};  
  
 // Handle Suit update form on PUT. 
 exports.suit_update_put = async function(req, res) { 
@@ -83,4 +92,61 @@ exports.suit_view_all_Page = async function(req, res) {
         res.status(500); 
         res.send(`{"error": ${err}}`); 
     }   
+}; 
+
+// Handle a show one view with id specified by query 
+exports.suit_view_one_Page = async function(req, res) { 
+    console.log("single view for id "  + req.query.id) 
+    try{ 
+        result = await Suit.findById( req.query.id) 
+        res.render('suitdetail',  
+{ title: 'suit Detail', toShow: result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+};
+
+ // Handle building the view for creating a suit. 
+// No body, no in path parameter, no query. 
+// Does not need to be async 
+exports.suit_create_Page =  function(req, res) { 
+    console.log("create view") 
+    try{ 
+        res.render('suitcreate', { title: 'suit Create'}); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
+
+
+// Handle building the view for updating a suit. 
+// query provides the id 
+exports.suit_update_Page =  async function(req, res) { 
+    console.log("update view for item "+req.query.id) 
+    try{ 
+        let result = await Suit.findById(req.query.id) 
+        res.render('suitupdate', { title: 'suit Update', toShow: result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
+
+// Handle a delete one view with id from query 
+exports.suit_delete_Page = async function(req, res) { 
+    console.log("Delete view for id "  + req.query.id) 
+    try{ 
+        result = await Suit.findById(req.query.id) 
+        res.render('suitdelete', { title: 'suit Delete', toShow: 
+result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
 }; 
--- routes/suit.js
+++ routes/suit.js
@@ -2,6 +2,20 @@ var express = require('express');
 const suit_controlers= require('../controllers/suit'); 
 var router = express.Router(); 
  
-/* GET costumes */ 
+/* GET suits */ 
 router.get('/', suit_controlers.suit_view_all_Page ); 
+
+/* GET detail suit page */ 
+router.get('/detail', suit_controlers.suit_view_one_Page); 
+
+/* GET create suit page */ 
+router.get('/create', suit_controlers.suit_create_Page); 
+
+/* GET create update page */ 
+router.get('/update', suit_controlers.suit_update_Page); 
+
+/* GET create suit page */ 
+router.get('/delete', suit_controlers.suit_delete_Page); 
+
+
 module.exports = router;
